9eaad286f352b535f59dd4ba3159f20c58068f15
loganofcarpenter <loganofcarpenter@gmail.com>
2015-12-02 13:05:44 -0700

    Delete velocity.h

--- Asteroids/Asteroids/velocity.h
+++
@@ -1,9 +0,0 @@
-#ifndef VELOCITY_H
-#define VELOCITY_H
-
-
-// Put your velocity class here, or overwrite this file with your own
-// velocity class from the previous project
-
-
-#endif /* velocity_h */
